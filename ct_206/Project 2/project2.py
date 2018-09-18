1aa4cca612d9702c4c7079ae9c9b72b3d31ef6ba
Kirin Patel <kirinpatel@gmail.com>
2018-09-18 19:36:00 -0400

    Added second part of project 2 (python)

--- ct_206/Project 2/project2.py
+++ ct_206/Project 2/project2.py
@@ -1,8 +1,8 @@
 def reverse_array(input_array):
-    # Get the length of the provided array
+    # Get the length of the provided array.
     length = len(input_array)
-    
-    # Initialize an array to the same length of the provided array
+
+    # Initialize an array to the same length of the provided array.
     output_array = [None]*length
 
     # Remove one from the calculated length of the input array.
@@ -15,5 +15,19 @@ def reverse_array(input_array):
     for (i, element) in enumerate(input_array):
         output_array[length - i] = element
 
-    # Returns output array
+    # Returns output array.
     return output_array
+
+
+def sum_array(input_array):
+    # Initialize a variable to hold the sum of the values within the
+    # provided array.
+    sum = 0
+
+    # Iterates through the input array, adding each value within the
+    # provided array to the sum variable, calculating the sum.
+    for x in input_array:
+        sum += x
+
+    # Returns the sum of all values within the provided array.
+    return sum
--- ct_206/Project 2/test.py
+++ ct_206/Project 2/test.py
@@ -5,9 +5,15 @@ class Tests(unittest.TestCase):
 
     def test_reverse(self):
         strings = ['abc', '123', 'abc123']
-        reversed_string = ['abc123', '123', 'abc']
-        self.assertEqual(reverse_array(strings), reversed_string)
+        reversed_strings = ['abc123', '123', 'abc']
+        self.assertEqual(reverse_array(strings), reversed_strings)
 
+    def test_sum_array(self):
+        a = [1, 2, 3]
+        b = [5, -7]
+        self.assertEqual(sum_array([]), 0)
+        self.assertEqual(sum_array(a), 6)
+        self.assertEqual(sum_array(b), -2)
 
 if __name__ == '__main__':
     unittest.main()
