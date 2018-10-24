58b58d87d0be83962409c97a60a9221cb2fd7aa7
Kirin Patel <kirinpatel@gmail.com>
2018-10-23 20:52:48 -0400

    Completed Master Page for CS 356

---
+++ cs_356/Master Page/Master Page/Login.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Master_Page
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        // Validates user login
+        protected void submitForm(object sender, EventArgs e)
+        {
+            // TODO: Handle user login, ensure email address and password match given requirements.
+        }
+    }
+}
