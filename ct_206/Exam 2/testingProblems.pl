b14c15b29606be74fde1957c09b59bc99033577e
Kirin Patel <kirinpatel@gmail.com>
2018-11-25 14:25:20 -0500

    Added Exam 2 tests

---
+++ ct_206/Exam 2/testingProblems.pl
@@ -0,0 +1,19 @@
+# $_ = "He's out bowling with George tonight."; s/George/Jim/; 
+# print "$_\ n";
+
+# my @fields = split /:/, "abc:def:g:h";
+# print @fields[1];
+
+# my $x = join ":", "a", "g", "f", "t", "y";
+# print $x;
+
+# my $x = join ":", 1, 3, 7, 10, 12; 
+# my @values = split /:/, $x;
+# my $z = join "-", @values;
+# printf $z
+
+# $string = "The cat sat on the mat";
+# $string =~ s/cat/dog/;
+# print "$string\n";
+
+# printf "%10.5f\n", 6*5 + 1/3;
