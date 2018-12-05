9f4b6b0c27f58450639cc6702f76c69cb0a92688
Kirin Patel <kirinpatel@gmail.com>
2018-12-04 19:30:54 -0500

    Changed date calculations

--- cs_356/Master Page/Master Page/Main.aspx.cs
+++ cs_356/Master Page/Master Page/Main.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -27,7 +27,7 @@ namespace Master_Page
 
         void Page_PreInit(object sender, EventArgs e)
         {
-            if (DateTime.Now.Month >= 11)
+            if (DateTime.Now.Month == 12)
             {
                 Page.Theme = "HolidayTheme";
             }
