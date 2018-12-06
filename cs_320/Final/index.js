919c9ebc0a3c18b5bcc540cc3186b157cbc05ba9
Kirin Patel <kirinpatel@gmail.com>
2018-12-06 09:06:58 -0500

    Started work on bootstrap.js

--- cs_320/Final/index.js
+++ cs_320/Final/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const bootstrap = require("./config/bootstrap");
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Service not setup.'));
+app.get("/", (req, res) => res.send("Service not setup."));
 
-app.listen(port, () => {
-  console.log(`Service started on port: ${port}`);
-});
+bootstrap
+  .validate()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Service started on port: ${port}`);
+    });
+  })
+  .catch(err => console.error(err));
