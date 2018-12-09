da602422ee35364bd392006751cc689a2f0893cd
Kirin Patel <kirinpatel@gmail.com>
2018-12-09 18:59:54 -0500

    Formatted files

--- cs_250/Project 2/NameResolution.c
+++ cs_250/Project 2/NameResolution.c
@@ -1,12 +1,13 @@
-#include <stdio.h> 	/* for printf/ fprintf */
-#include <netdb.h> 	/* for gethostbyname() */ 
+#include <stdio.h> /* for printf/ fprintf */
+#include <netdb.h> /* for gethostbyname() */
 
 void DieWithError(char *errorMessage);
-unsigned long NameResolution(char name[]) {
-	struct hostent *host; 	/* Structure containing host information */
+unsigned long NameResolution(char name[])
+{
+	struct hostent *host; /* Structure containing host information */
 	if ((host = gethostbyname(name)) == NULL)
 		DieWithError("gethostbyname() failed ");
 
-/* return the binary, network-byte-ordered address */
-return * ((unsigned long *) host->h_addr_list [ 0] ) ;
+	/* return the binary, network-byte-ordered address */
+	return *((unsigned long *)host->h_addr_list[0]);
 }
