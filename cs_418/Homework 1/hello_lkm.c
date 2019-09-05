d68d10873524fc1db342cae04da8075d804ff30a
Kirin Patel <kirinpatel@gmail.com>
2019-09-05 19:57:10 -0400

    Added Homework 1 files

---
+++ cs_418/Homework 1/hello_lkm.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+
+int init_module(void)
+{
+	printk(KERN_INFO "cs418: Hello world \n");
+
+	/* 
+	 * A non 0 return means init_module failed; module can't be loaded. 
+	 */
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "cs418: Great job!! Goodbye \n");
+}
